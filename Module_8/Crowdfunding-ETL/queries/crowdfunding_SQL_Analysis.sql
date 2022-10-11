d909665340688fd1e1e7afc9087ef308532189ba
JamesonThornton <JamesonThornton@Gmail.com>
2022-10-10 23:05:59 -0400

    Initial Module 8 upload, 90% complete on challenge

---
+++ Module_8/Crowdfunding-ETL/queries/crowdfunding_SQL_Analysis.sql
@@ -0,0 +1,40 @@
+-- Challenge Bonus queries.
+-- 1. (2.5 pts)
+-- Retrieve all the number of backer_counts in descending order for each `cf_id` for the "live" campaigns. 
+SELECT c.cf_id,
+	SUM(c.backers_count)
+FROM campaign as c
+WHERE c.outcome='live'
+GROUP BY c.cf_id
+ORDER BY sum DESC;
+
+
+-- 2. (2.5 pts)
+-- Using the "backers" table confirm the results in the first query.
+SELECT b.cf_id,
+	COUNT(b.backer_id) as count
+FROM backers as b
+GROUP BY b.cf_id 
+ORDER BY count DESC;
+
+
+-- 3. (5 pts)
+-- Create a table that has the first and last name, and email address of each contact.
+-- and the amount left to reach the goal for all "live" projects in descending order. 
+
+
+
+
+-- Check the table
+
+
+-- 4. (5 pts)
+-- Create a table, "email_backers_remaining_goal_amount" that contains the email address of each backer in descending order, 
+-- and has the first and last name of each backer, the cf_id, company name, description, 
+-- end date of the campaign, and the remaining amount of the campaign goal as "Left of Goal". 
+
+
+
+-- Check the table
+
+
---
+++ Module_8/README.md
@@ -0,0 +1,22 @@
+# Module 8 - Extract, Trasnfer, Load for "Independant Funding"
+
+## Project Overview
+Use Python, Pandas, and Jupyter notebooks to do the extract and transform phases. Specifically, I will extract and transform the backers’ contact information from a CSV file to create a DataFrame that will be exported as a CSV file. I'll then do the load phase. Specifically, I’ll use the dataset to create an ERD and a table schema for creating a new table in the crowdfunding_db database. And, I’ll upload the CSV file that contains the backers’ information into this table. Finally, I’ll perform a data analysis on the crowdfunding_db database by using SQL queries.
+
+### Expected steps
+
+
+#### Deliverables
+Deliverable 1: Extract Data
+Deliverable 2: Transform and Clean Data
+Deliverable 3: Create an ERD and Table Schema, and Load Data
+Deliverable 4: SQL Analysis
+
+## Challenges
+initially forgot to export the backers table without an index. initially read into SQL with an error. corrected it and reloaded. 
+
+## Data Summary Results
+
+
+## Conclusion
+
