01fb8705fee40219e674cf1ef66e8c449f574efe
JamesonThornton <JamesonThornton@Gmail.com>
2022-11-20 15:33:46 -0500

    added layers control

--- Module_14/Mapping_Earthquakes/static/js/logic.js
+++ Module_14/Mapping_Earthquakes/static/js/logic.js
@@ -2,14 +2,16 @@
 console.log("working");
 
 
-// Create the map object with a center and zoom level.
-let map = L.map("mapid", {
-    center: [
-      40.7, -94.5
-    ],
-    zoom: 4
-  });
+// // Create the map object with a center and zoom level.
+// let map = L.map("mapid", {
+//     center: [
+//       40.7, -94.5
+//     ],
+//     zoom: 4
+//   });
 
+  // Create the map object with center and zoom level.
+let map = L.map('mapid').setView([30, 30], 2);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -20,3 +22,13 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+// Accessing the airport GeoJSON URL
+let airportData = https://raw.githubusercontent.com/JamesonThornton/UCB_Data_bootcamp/Mapping_GeoJSON_Points/Module_14/Mapping_Earthquakes/Mapping_GeoJSON_Points/static/js/majorAirports.json
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data).addTo(map);
+});
