a665214ee48c423b5857033892ab4b6114b4b144
JamesonThornton <JamesonThornton@Gmail.com>
2022-11-20 21:30:32 -0500

    connected USGS json

---
+++ Module_14/Mapping_Earthquakes/Earthquakes_past7days/static/js/logicStep1.js
@@ -0,0 +1,44 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+
+
+
+// We create the dark view tile layer that will be an option for our map.
+let street = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox/streets-v11',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// We create the dark view tile layer that will be an option for our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox/satellite-streets-v11',
+    // id: 'mapbox/streets-v11',
+    maxZoom: 18,
+    accessToken: API_KEY
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  "Streets": street,
+  "Satellite Streets": satelliteStreets
+};
+
+
+// Create the map object with a center and zoom level.
+let map = L.map("mapid", {
+  center: [39.5, -98.5],
+  zoom: 3
+  layers: [street]
+});
+
+L.control.layers(baseMaps).addTo(map)
+
+let quakes_7Day = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+d3.json(quakes_7Day).then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
+});
